32891468878576c3930ced466485938bef735be6
Kyle Talmage <ktalmage85@gmail.com>
2020-02-18 14:02:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,18 @@
 class Person
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def jobs=(jobs)
+    @jobs = jobs
+  end
+  
+  def jobs
+    @jobs
+  end
 end
